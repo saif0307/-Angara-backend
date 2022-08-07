7c746be60f9bfcba1eb8475c8ad6b35511276df9
saif0307 <saif.1077@yahoo.com>
2022-08-07 22:31:51 +0500

    Changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const databaseName = "angara-portal-api";
-const URL = `mongodb://127.0.0.1:27017/${databaseName}`;
+// const URL = `mongodb://127.0.0.1:27017/${databaseName}`;
+const URL = `mongodb+srv://m_saif1999:LX3PEcapIv1Gt6Du@cluster1.s10juvx.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
 
 mongoose.connect(
 	URL,
